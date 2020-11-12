338da161c2f73bab4b4641bf4ee80ed78047c6f1
Dmitry Romashov <framebassman@gmail.com>
2020-11-13 01:40:24 +0300

    try to break

--- src/tech/romashov/domain/ApplicationStand.groovy
+++ src/tech/romashov/domain/ApplicationStand.groovy
@@ -5,7 +5,7 @@ import tech.romashov.BuildContext
 class ApplicationStand {
     private static final Script SCRIPT = BuildContext.script
 
-    public ApplicationStand() {}
+    ApplicationStand() {}
 
     void build() {
         SCRIPT.sh('I want to build Application')
---
+++ test/pipelines/tech/romashov/dummy/ApplicationPipelineTest6.groovy
@@ -0,0 +1,136 @@
+package pipelines.tech.romashov
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.Before
+import org.junit.Test
+
+class ApplicationPipelineTest6 extends BasePipelineTest {
+    @Override
+    @Before
+    void setUp() throws Exception {
+        super.setUp()
+        helper.registerAllowedMethod('echo', [String.class])
+    }
+
+        @Test
+    void testMethod0() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod1() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod2() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod3() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod4() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod5() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod6() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod7() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod8() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod9() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod10() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod11() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod12() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod13() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod14() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod15() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod16() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod17() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod18() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void testMethod19() throws Exception {
+        runScript("pipelines/tech/romashov/ApplicationPipeline.groovy")
+        assertJobStatusSuccess()
+    }
+
+
+}
