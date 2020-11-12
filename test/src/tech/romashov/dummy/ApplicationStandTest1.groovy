fefaa7beaa8b117512167975ad7e8c1b6f9b810a
Dmitry Romashov <framebassman@gmail.com>
2020-11-13 01:43:20 +0300

    add tests

---
+++ test/src/tech/romashov/dummy/ApplicationStandTest1.groovy
@@ -0,0 +1,128 @@
+package src.tech.romashov.dummy
+
+import org.junit.Test
+import tech.romashov.domain.ApplicationStand
+
+class ApplicationStandTest1 {
+        @Test
+    void testMethod0() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod1() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod2() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod3() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod4() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod5() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod6() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod7() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod8() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod9() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod10() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod11() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod12() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod13() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod14() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod15() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod16() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod17() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod18() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+    @Test
+    void testMethod19() {
+        ApplicationStand stand = new ApplicationStand()
+        stand.build()
+    }
+
+
+}
